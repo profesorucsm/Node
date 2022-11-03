c1fcf09e574877a72cfa09df2c92ebd16a23ae60
profesorucsm <msantillana@ucsm.edu.pe>
2022-11-03 14:34:56 -0500

    02 Conexion con MySQL

---
+++ testingmysql/testing.js
@@ -0,0 +1,21 @@
+
+// Solicitando paquete de Mysql
+var mysql = require('mysql');
+
+// Configurando parámetros de conexión
+var conexion = mysql.createConnection({
+    host:'localhost',
+    port:'8889',
+    database: 'musicdb',
+    user: 'root',
+    password: 'root',
+});
+
+// Realizando conexión o verificando si sucedió un error
+conexion.connect(function(err){
+    if(err){
+        console.log("Error de conexion"+ err.stack);
+        return;
+    }
+    console.log("Conectado al ID "+conexion.threadId);
+});
